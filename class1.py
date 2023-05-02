47960c14d6456c5d56ea47756a9e42660d49349e
xtwsvktr <xtwsvktr@gmail.com>
2023-05-02 14:33:34 +0600

    hw8

--- class1.py
+++ class1.py
@@ -1,3 +1,9 @@
 class Hello:
     def __init__(self, str):
         self.str = str
+
+        class Hi(Hello):
+
+            def __str__(self):
+                return f'Hello {self.str}'
+
--- task.py
+++ task.py
@@ -1,2 +1,8 @@
 from art import tprint
 tprint("PYTHON")
+
+from class1 import Hi
+
+name = Hi('Andrey')
+
+tprint(str(name))
